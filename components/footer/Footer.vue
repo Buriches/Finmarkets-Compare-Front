2a7a384906213cbc8372ba01f5093c6bfcaf65fe
Buriches <01121998kiril@gmail.com>
2023-06-06 03:46:39 +0300

    feat: final layout version

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -10,24 +10,26 @@
       <NuxtLink to="/about">About us</NuxtLink>
       <NuxtLink to="/">Store</NuxtLink>
     </div>
-    <div class="footer-search">
+    <form class="footer-search" name="footer-search" @submit.prevent>
       <input type="text" placeholder="Search">
-      <Icon
-        name="iconamoon:search-thin"
-        color="#468966"
-        width="20"
-        height="20"
-        :rotate="1"
-        :verticalFlip="true"
-      />
-    </div>
+      <button type="submit">
+        <Icon
+          name="iconamoon:search-thin"
+          color="#468966"
+          width="20"
+          height="20"
+          :rotate="1"
+          :verticalFlip="true"
+        />
+      </button>
+    </form>
     <div class="footer-media">
-      <p>We are in social networks</p>
+      <p class="footer-media-title">We are in social networks</p>
       <div class="footer-media-list">
-        <div class="footer-media-list-item"><Icon icon="cib:instagram" color="#dadada" width="15" height="15" /></div>
-        <div class="footer-media-list-item"><Icon icon="ri:facebook-fill" color="#dadada" width="15" height="15" /></div>
-        <div class="footer-media-list-item"><Icon icon="ic:baseline-wechat" color="#dadada" width="15" height="15" /></div>
-        <div class="footer-media-list-item"><Icon icon="basil:telegram-solid" color="#dadada" width="15" height="15" /></div>
+        <div class="footer-media-list-item"><a href="https://www.instagram.com/"><Icon name="cib:instagram" color="#dadada" width="17" height="17" /></a></div>
+        <div class="footer-media-list-item"><a href="https://facebook.com/"><Icon name="ri:facebook-fill" color="#dadada" width="20" height="20" /></a></div>
+        <div class="footer-media-list-item"><a href="https://www.wechat.com/"><Icon name="ic:baseline-wechat" color="#dadada" width="20" height="20" /></a></div>
+        <div class="footer-media-list-item"><a href="https://web.telegram.org/"><Icon name="basil:telegram-solid" color="#dadada" width="18" height="18" /></a></div>
       </div>
     </div>
   </div>
@@ -90,6 +92,60 @@
       &:hover{
         color: $COLOR_HOVER;
         border-bottom: 1px solid $COLOR_HOVER;
+      }
+    }
+  }
+  &-search{
+    display: flex;
+    width: 395px;
+    height: 40px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    align-items: center;
+    justify-content: space-between;
+    & input{
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 17px;
+      color: #777777;
+      border: 0;
+      outline: none;
+      width: 100%;
+      padding-left: 20px;
+    }
+    & button{
+      padding-right: 20px;
+      height: 100%;
+      padding-left: 5px;
+      cursor: pointer;
+      outline: none;
+      border: 0;
+      background-color: transparent;
+    }
+  }
+  &-media{
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
+    &-title{
+      font-weight: 300;
+      font-size: 16px;
+      line-height: 20px;
+      color: $COLOR_TEXT_PRIMARY;
+    }
+    &-list{
+      display: flex;
+      column-gap: 15px;
+      justify-content: space-between;
+      &-item{
+        width: 35px;
+        height: 35px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: $COLOR_PRIMARY;
+        border-radius: 50%;
+        cursor: pointer;
       }
     }
   }
